992e09f3511d005886bec34fc76d676e74dc3d85
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-10-30 19:24:03 +0600

    Added Active button functions

--- src/components/Available/Available.jsx
+++ src/components/Available/Available.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 
 const Available = () => {
+    const [activeButton, setActiveButton] = useState(1);
+
+    const handleActiveButton = (button) => {
+        setActiveButton(button);
+    }
+
+
     return (
         <div className="w-11/12 mx-auto mt-16 flex justify-between">
             <h2 className="card-title">Available Players</h2>
             <div className="flex border rounded-full overflow-hidden">
-                <button className="btn bg-[#E7FE29] font-bold border-[#e0e0e2] rounded-l-xl rounded-r-none">Available</button>
-                <button className="btn bg-white border text-[#13131399] border-[#e0e0e2] rounded-l-none rounded-r-xl">Selected (0)</button>
+                <button onClick={() => handleActiveButton(1)} className={`btn font-bold border-[#e0e0e2] rounded-l-xl rounded-r-none ${
+                    activeButton === 1 ? 'bg-[#E7FE29]' : 'bg-white'
+                }`}>Available</button>
+                <button onClick={() => handleActiveButton(2)} className={`btn border text-[#13131399] border-[#e0e0e2] rounded-l-none rounded-r-xl ${
+                    activeButton === 2 ? 'bg-[#E7FE29]' : 'bg-white'
+                }`}>Selected (0)</button>
             </div>
         </div>
     );
